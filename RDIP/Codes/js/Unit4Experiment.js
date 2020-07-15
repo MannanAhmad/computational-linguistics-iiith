c5b715df3ba4802ca3ea75b7cfb3a7bb7266183c
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-07-16 02:24:11 +0530

    Adding an algorithm to find unique words after stemming GOAL-9 Unit 4

--- RDIP/Codes/js/Unit4Experiment.js
+++ RDIP/Codes/js/Unit4Experiment.js
@@ -99,7 +99,7 @@ function checkAns() {
 
         //excluding newline with a start spacing
         token = token.replace(/\n /, "\n");
-        
+
         //checking no of tokens
         if (document.getElementById("token").value == token.split(' ').length) {
 
@@ -115,8 +115,8 @@ function checkAns() {
         // count unique words
 
         const countUniqueWords = s => new Set(s.toLowerCase().match(/\w+/g)).size
-
         
+
         // alert(`A has ${countUniqueWords(a)} unique words`);
         if (document.getElementById("types").value == countUniqueWords(token)) {
 
@@ -254,46 +254,71 @@ function rootWord() {
     document.getElementById("msg").innerText = "Now, consider all the tokens with the same 'root' word to be of the same type. Recalculate the number of types."
 }
 
-// Function to check new types
+
+//checking Unique words for new corpus tokens
+function uniqueWordForStemmedCorpus(word, position, self) { 
+    return self.indexOf(word) === position;
+}
+
+// Counting total uniques words for new corpus
 function againCheckTypes() {
-    if (selection.value === 'corp1'){
-    let token = 'A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin. At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the hole. Then she began to eat the corn. Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat before she felt that she had had enough. When the mouse tried to climb out of the basket, she could not. She was too fat to pass through the hole. "How shall I climb out?" said the mouse. "oh, how shall I climb out?" Just then a rat came along, and he heard the mouse. "Mouse," said the rat, "if you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in."'
-    token = token.replace(/[^a-zA-Z ]/g, "");
-    token = token.toLowerCase();
-    token = token.split(" ");
-    let types = [];
-    for (let i = 0;  i<= token.length-1; i++) {
-        stemmer.setCurrent(token[i]);
-        stemmer.stem();
-        types.push(stemmer.getCurrent());
+    if (selection.value === 'corp1') {
+        let token = 'A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin. At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the hole. Then she began to eat the corn. Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat before she felt that she had had enough. When the mouse tried to climb out of the basket, she could not. She was too fat to pass through the hole. "How shall I climb out?" said the mouse. "oh, how shall I climb out?" Just then a rat came along, and he heard the mouse. "Mouse," said the rat, "if you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in."'
+        token=token.replace(/[^a-zA-Z ]/g, "");
+        token = token.replace(/(^\s*)|(\s*$)/gi, "");
+        token = token.replace(/[ ]{2,}/gi, " ");
+        token = token.replace(/\n /, "\n");
+        token = token.toLowerCase();
+        token = token.split(" ");
+        let types = [];
+        for (let i = 0; i <token.length; i++) {
+            stemmer.setCurrent(token[i]);
+            stemmer.stem();
+            types.push(stemmer.getCurrent());
         }
-    // console.log(types);
+        console.log(types);
+        let finalTypes = types.filter(uniqueWordForStemmedCorpus);
+        console.log(finalTypes)
+        
+        
     }
-   else if (selection.value === 'corp2'){
-    let token = 'A wolf carried off a lamb. The lamb said, "I know you are going to eat me, but before you eat me I would like to hear you play the flute. I have heard that you can play the flute better than anyone else, even the shepherd himself." The wolf was so pleased at this that he took out his flute and began to play. When he had done, the lamb insisted him to play once more and the wolf played again. The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock.'
-    token =token.replace(/[^a-zA-Z ]/g, "");
-    token = token.toLowerCase();
-    token = token.split(" ");
-    let types = [];
-    for (var i = 0; i <= token.length-1; i++) {
-        stemmer.setCurrent(token[i]);
-        stemmer.stem();
-        types.push(stemmer.getCurrent());
+    else if (selection.value === 'corp2') {
+        let token = 'A wolf carried off a lamb. The lamb said, "I know you are going to eat me, but before you eat me I would like to hear you play the flute. I have heard that you can play the flute better than anyone else, even the shepherd himself." The wolf was so pleased at this that he took out his flute and began to play. When he had done, the lamb insisted him to play once more and the wolf played again. The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock.'
+        token=token.replace(/[^a-zA-Z ]/g, "");
+        token = token.replace(/(^\s*)|(\s*$)/gi, "");
+        token = token.replace(/[ ]{2,}/gi, " ");
+        token = token.replace(/\n /, "\n");
+        token = token.toLowerCase();
+        token = token.split(" ");
+        let types = [];
+        for (let i = 0; i <token.length; i++) {
+            stemmer.setCurrent(token[i]);
+            stemmer.stem();
+            types.push(stemmer.getCurrent());
         }
-    // console.log(types);
+        console.log(types);
+        let finalTypes = types.filter(uniqueWordForStemmedCorpus);
+        console.log(finalTypes)
+        
     }
-   else if (selection.value === 'corp3'){
-    var token = 'A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. "Why does he not make a pet of me?" said the donkey. "It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master\'s knee. It is not fair." Then the donkey said to himself, "If I do what the dog does, he may make a pet of me." So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master\'s knee. The master thought the donkey was mad, and he shouted, "Help! Help!" Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field. "I only did what the dog does," said the donkey," and yet they make a pet of the dog, and they beat me with sticks. It is not fair.'
-    token = token.replace(/[^a-zA-Z ]/g, "");
-    token = token.toLowerCase();
-    token = token.split(" ");
-    var types = [];
-    for (let i = 0; i <= token.length-1; i++) {
-        stemmer.setCurrent(token[i]);
-        stemmer.stem();
-        types.push(stemmer.getCurrent());
+    else if (selection.value === 'corp3') {
+        var token = 'A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. "Why does he not make a pet of me?" said the donkey. "It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master\'s knee. It is not fair." Then the donkey said to himself, "If I do what the dog does, he may make a pet of me." So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master\'s knee. The master thought the donkey was mad, and he shouted, "Help! Help!" Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field. "I only did what the dog does," said the donkey," and yet they make a pet of the dog, and they beat me with sticks. It is not fair.'
+        token=token.replace(/[^a-zA-Z ]/g, "");
+        token = token.replace(/(^\s*)|(\s*$)/gi, "");
+        token = token.replace(/[ ]{2,}/gi, " ");
+        token = token.replace(/\n /, "\n");
+        token = token.toLowerCase();
+        token = token.split(" ");
+        let types = [];
+        for (let i = 0; i <token.length; i++) {
+            stemmer.setCurrent(token[i]);
+            stemmer.stem();
+            types.push(stemmer.getCurrent());
         }
-    // console.log(types);
+        console.log(types);
+        let finalTypes = types.filter(uniqueWordForStemmedCorpus);
+        console.log(finalTypes)
+        
     }
 
 
